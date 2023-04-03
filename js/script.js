f126c7ae5dfbb07a8dfac5736b27f13e74ae4ef6
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-04-03 08:18:29 -0300

    textos adicionados no html

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ fetch('Website_text/sobre.txt')
                     document.getElementById('texto_sobre').innerHTML = text;
                 });
 
+// Fetch the text file
+fetch('Website_text/Projetos.txt')
+                .then(response => response.text())
+                .then(text => {
+                    document.getElementById('texto_projetos').innerHTML = text;
+                });
+// Fetch the text file
+fetch('Website_text/noticias.txt')
+                .then(response => response.text())
+                .then(text => {
+                    document.getElementById('texto_noticias').innerHTML = text; 
+                });
+
+// Fetch the text file
+fetch('Website_text/Contato.txt')
+                .then(response => response.text())
+                .then(text => {
+                    document.getElementById('texto_contato').innerHTML = text;
+                });
+
 
 // Carrousel AUTO-NAVIGATION
 var counter = 1;
