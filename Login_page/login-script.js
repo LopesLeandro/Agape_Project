cb1fd0ed5add47f50add64c3ddf2209a9390ca5e
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-04-18 08:52:41 -0300

    disabled button

---
+++ Login_page/login-script.js
@@ -0,0 +1,17 @@
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
+
+function isFormValid() {
+  return usernameInput.value !== "" && passwordInput.value !== "";
+}
+
+loginButton.disabled = true;
+
+usernameInput.addEventListener("input", function() {
+  loginButton.disabled = !isFormValid();
+});
+
+passwordInput.addEventListener("input", function() {
+  loginButton.disabled = !isFormValid();
+});
